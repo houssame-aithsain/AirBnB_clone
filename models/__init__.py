fde9f6c1a2beda251907774e7bcc415e4252de5a
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2024-05-19 14:45:40 +0000

    base

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""
+Module: __init__.py
+"""
+from models.engine import file_storage
 
-''' Import the FileStorage class from the engine module '''
 
-from .engine.file_storage import FileStorage
-''' Create a FileStorage instance '''
-
-storage = FileStorage()
+"""Create a FileStorage instance to manage the storage of the application"""
+storage = file_storage.FileStorage()
 storage.reload()
