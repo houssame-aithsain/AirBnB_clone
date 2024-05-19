9450c97cc82ad25cadf258c3dd3c0bad58e15e04
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2024-05-19 18:08:19 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -19,18 +19,14 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
-        if kwargs:
+        if len(kwargs) > 0:
             for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.today()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.today()
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
