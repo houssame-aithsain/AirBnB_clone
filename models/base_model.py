61c1e874f246fa62e9b9b5f3596be1eed75e3d80
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2024-05-19 16:10:19 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -23,12 +23,6 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            if "id" not in kwargs.keys():
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs.keys():
-                self.created_at = datetime.today()
-            if "updated_at" not in kwargs.keys():
-                self.updated_at = datetime.today()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.today()
