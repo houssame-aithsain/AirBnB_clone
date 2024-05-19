e20fa354f3dcedd7d4cb73ddfb0b17e1ee7f68b8
Houssame Ait hsain <88860095+houssame-aithsain@users.noreply.github.com>
2024-05-19 14:54:13 +0000

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Module: file_storage.py
-Defines a `FileStorage` class.
-"""
+# -*- coding: utf-8 -*-
+"""This module defines a class to manage file storage for hbnb project"""
 
 import os
 import json
@@ -15,34 +13,28 @@ from models.amenity import Amenity
 from models.place import Place
 
 
-class FileStorage():
-    """
-    serializes instances to a JSON file and
-    deserializes JSON file to instances
-    """
+class FileStorage:
+    """This class manages storage of JSON file"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        returns the dictionary __objects
-        """
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """
-        sets in __objects the obj with key <obj class name>.id
-        """
-        key = "{}.{}".format(type(obj).__name__, obj.id)
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """
-        serializes __objects to the JSON file (path: __file_path)
-        """
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(
-                {k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding='utf-8') as f:
+            json.dump(new_dict, f)
 
     def reload(self):
         """
